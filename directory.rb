9ba623da5d929ca3848bb716b37f6ca8fd1d132d
toppy007 <chris_t72@hotmail.com>
2023-03-02 16:37:39 +0000

    methods added + student_input.

--- directory.rb
+++ directory.rb
@@ -1,21 +1,45 @@
-student_count = 11
 # Array of students
-students = [ "Dr. Hannibal Lecter", 
-            "Darth Vader", 
-            "Nurse Ratched",
-            "Michael Corleone",
-            "Alex DeLarge",
-            "The Wicked Witch of the West",
-            "Terminator",
-            "Freddy Krueger", 
-            "The Joker",
-            "Joffrey Baratheon",
-            "Norman Bates" ]
-# print them with .each in i elements of student array
-puts "The students of Villains Academy"
-puts "-------------"
-students.each do |i|
-    puts i
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create empty array 
+  students = []
+  # get input
+  name = gets.chomp
+  # while name is empty run while command
+  while !name.empty? do
+    # hash name into students array
+    students << {name: name, cohort: :november}
+    # code to show latest student count
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  students
 end
+            
+# print them with .each in i elements of student array
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(students)
+  students.each do |i|
+    puts "#{i[:name]} (#{i[:cohort]} cohort)"
+  end
+end
+
 # Overal student count using .count on students array
-puts "Overall, we have #{students.count} great students"
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
+end
+
+# call the methods
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
