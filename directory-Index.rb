6afc6092206d551f9a186f249cc3c2c0c2e28cf1
toppy007 <chris_t72@hotmail.com>
2023-03-02 17:31:03 +0000

    added index task and first letter task file

---
+++ directory-Index.rb
@@ -0,0 +1,45 @@
+# Array of students
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create empty array 
+  students = []
+  # get input
+  name = gets.chomp
+  # while name is empty run while command
+  while !name.empty? do
+    # hash name into students array
+    students << {name: name, cohort: :november}
+    # code to show latest student count
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  students
+end
+            
+# print them with .each in i elements of student array
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(students)
+  students.each.with_index(1) do |i, index|
+    puts "#{index}: #{i[:name]} (#{i[:cohort]} cohort)"
+  end
+end
+
+# Overal student count using .count on students array
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
+end
+
+# call the methods
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
